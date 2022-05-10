e93fb5318908507d91c24ba51532277fa7fb59ea
datasandwich <72459044+datasandwich@users.noreply.github.com>
2022-05-10 16:45:57 +0100

    Part 1 complete.

--- src/Person.java
+++ src/Person.java
@@ -1,5 +1,46 @@
 public class Person {
-    public static void main(String[] args) {
+    String name;
+    int age;
+    double height;
+    double weight;
 
+    public Person(String name, int age, double height, double weight) {
+        setName(name);
+        setAge(age);
+        setHeight(height);
+        setWeight(weight);
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
 }
---
+++ src/PersonCreator.java
@@ -0,0 +1,16 @@
+public class PersonCreator {
+    public static void main(String[] args) {
+        Person Person01 = new Person("James Peterson",34,180,73.5);
+        Person Person02 = new Person("Bob Thornley",25,171,68.2);
+
+        System.out.println(Person01.getName());
+        System.out.println(Person01.getAge());
+        System.out.println(Person01.getHeight());
+        System.out.println(Person01.getWeight());
+
+        System.out.println(Person02.getName());
+        System.out.println(Person02.getAge());
+        System.out.println(Person02.getHeight());
+        System.out.println(Person02.getWeight());
+    }
+}
