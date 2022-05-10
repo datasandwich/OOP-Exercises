8dbf83b073819200627ee5d4e01db351b810ed84
datasandwich <72459044+datasandwich@users.noreply.github.com>
2022-05-10 17:04:31 +0100

    Part 3 complete.

---
+++ src/CircleTest.java
@@ -0,0 +1,10 @@
+public class CircleTest {
+    public static void main(String[] args) {
+        Circle c1 = new Circle(2.3,"blue");
+        System.out.println(c1.getColor());
+        System.out.println(c1.getRadius()+"cm");
+        System.out.println(c1.getArea()+"cm");
+
+
+    }
+}
